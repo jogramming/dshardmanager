f783445a82de39f78e8d423abeaa6f26aa300052
jonas747 <jonasr747@gmail.com>
2018-02-13 02:52:59 +0100

    Avoid an extra sleep when starting

--- dshardmanager.go
+++ dshardmanager.go
@@ -149,14 +149,17 @@ func (m *Manager) Start() error {
 
 	m.Sessions = make([]*discordgo.Session, m.numShards)
 	for i := 0; i < m.numShards; i++ {
+		if i != 0 {
+			// One indentify every 5 seconds
+			time.Sleep(time.Second * 5)
+		}
+
 		m.Lock()
 		err := m.startSession(i)
 		m.Unlock()
 		if err != nil {
 			return errors.WithMessage(err, fmt.Sprintf("Failed starting shard %d", i))
 		}
-		// One indentify every 5 seconds
-		time.Sleep(time.Second * 5)
 	}
 
 	return nil
